da8e9e3e1da1d8030976be43131b8158f671bf42
Frank Kong <frank@pop-os.localdomain>
2022-03-26 22:25:54 -0400

    set up react typescript tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+      <span className='text-sky-300 text-6xl'>Hello</span>
+    </React.Fragment>
+  );
+}
+
+export default App;
